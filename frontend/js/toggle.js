b67fe11104b95cc0595b2a73ac800672a27717c4
Meenakshi <janagammeenakshi62@gmail.com>
2026-06-20 11:12:11 +0530

    task completed

--- frontend/js/toggle.js
+++ frontend/js/toggle.js
@@ -1,26 +1,31 @@
-var users =[
+var users = [
     {
         "name": "John Doe",
-        "gender":" male",
-        "image": "/images/john.png"
+        "gender": "Male",
+        "image": "../images/john.png"
     },
     {
         "name": "Jane Doe",
-        "gender":" female",
-        "image": "/images/jane.png"
+        "gender": "Female",
+        "image": "../images/jane.png"
     }
 ]
+
 var curId = 0;
-function toggle(){
-    //toggle curId from 0 ->1 & 1->0
-    curId =(curdId + 1) % 2;
-    /*toggle the rendered user  details*/
-    //image
-    var user=users[curId];
-    document.getElementById("user-img").src=user.image;
-    //to do : update for name and gender !
-    //name
-    document.getElementById("user-name").innerText=user.name;
-    //gender
-    document.getElementById("user-gender").innerText=user.gender;
+function toggle() {
+    // curId = (curId + 1) % users.length;
+    curId = (curId + 1) % 2;
+
+    // toggle the rendered user details
+
+    // image
+    var user = users[curId];
+    document.getElementById("user-img").src = user.image;
+
+    // name
+    document.getElementById("user-name").innerText = user.name;
+
+    // gender
+    document.getElementById("user-gender").innerText = user.gender;
+   // document.getElementById("user-gender").textContent = user.gender;
 }
